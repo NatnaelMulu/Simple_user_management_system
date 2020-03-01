0adb4e1306a5b7af0590db0edb13ff4f4667a122
Dereje3030 <51210918+Dereje3030@users.noreply.github.com>
2020-03-01 23:06:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/simple_user_management_system/UserDetailActivity.java
@@ -0,0 +1,39 @@
+package com.example.simple_user_management_system;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.user_managment.util.User;
+
+public class UserDetailActivity extends AppCompatActivity {
+    TextView tvFullName;
+    TextView tvMail;
+    TextView tvPhone;
+    TextView tvGender;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_detail);
+        getActionBar().setTitle("User Detail");
+        Intent intent=getIntent();
+        Bundle bundle=intent.getExtras();
+        User user=(User)bundle.get("user");
+
+        tvFullName=findViewById(R.id.fullName);
+        tvMail=findViewById(R.id.email);
+        tvPhone=findViewById(R.id.mobile);
+        tvGender=findViewById(R.id.gender);
+
+        tvFullName.setText(user.getFullName());
+        tvMail.setText(user.getEmail());
+        tvGender.setText(user.getGender());
+        tvPhone.setText(user.getPhone());
+
+
+    }
+}
+
